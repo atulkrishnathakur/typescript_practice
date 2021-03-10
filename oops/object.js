1fc21ad79ed0e1f6126acec85400bc1c6e7f38c8
Atul Krishna Thakur <gitvcs@gmail.com>
2021-03-10 08:06:31 +0530

    create object in typescript

---
+++ oops/object.js
@@ -0,0 +1,10 @@
+var Employee = /** @class */ (function () {
+    function Employee() {
+    }
+    return Employee;
+}());
+var emp_obj = new Employee();
+emp_obj.empCode = 101;
+emp_obj.empName = "Atul Krishna Thakur";
+console.log("Employee Code:" + emp_obj.empCode);
+console.log("Employee Name:" + emp_obj.empName);
---
+++ oops/object.ts
@@ -0,0 +1,16 @@
+class Employee{
+
+   empCode: number;
+   empName: string;
+
+}
+
+
+var emp_obj = new Employee();
+
+emp_obj.empCode = 101;
+
+emp_obj.empName = "Atul Krishna Thakur";
+
+console.log("Employee Code:"+emp_obj.empCode);
+console.log("Employee Name:"+emp_obj.empName);
